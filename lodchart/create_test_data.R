b6b4ad826281968939eae241d1867f751f4696e3
Karl Broman <kbroman@gmail.com>
2013-05-24 21:59:56 -0500

    lodchart: revise example data to include two lod curves

--- lodchart/create_test_data.R
+++ lodchart/create_test_data.R
@@ -3,7 +3,9 @@
 library(qtl)
 data(hyper)
 hyper <- calc.genoprob(hyper, step=1)
-out <- scanone(hyper, chr=1:6)
+out.em <- scanone(hyper, chr=1:6)
+out.hk <- scanone(hyper, chr=1:6, method="hk")
+out <- cbind(out.em, out.hk, labels=c("em", "hk"))
 
 source("scanone2json.R")
 cat(scanone2json(out), file="scanone.json")
