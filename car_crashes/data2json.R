26e7e801ade6a6249d793f7da936d0b0af58ca8e
Karl Broman <kbroman@gmail.com>
2014-10-30 14:52:20 -0500

    Beginning work on car_crashes example
     - Interactive graph of data from 538 blog post about car crash
       statistics by state,
       http://fivethirtyeight.com/datalab/which-state-has-the-worst-drivers/

---
+++ car_crashes/data2json.R
@@ -0,0 +1,12 @@
+# create JSON file with car crash data
+# (from http://fivethirtyeight.com/datalab/which-state-has-the-worst-drivers/)
+
+# read data (read off figs by hand, with a ruler)
+source("data.R")
+
+# convert to json format
+library(jsonlite)
+
+crashes$state <- rownames(crashes)
+rownames(crashes) <- 1:nrow(crashes)
+cat(toJSON(crashes, dataframe="columns"), file="data.json")
