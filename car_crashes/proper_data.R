20b9dc443311cc6997d8914af05cc7c5273675f8
Karl Broman <kbroman@gmail.com>
2014-11-03 13:12:19 -0600

    car_crashes: Hadn't included state abbreviations

--- car_crashes/proper_data.R
+++ car_crashes/proper_data.R
@@ -1,3 +1,7 @@
+# my original measurements
+source("data.R")
+abbrev <- crashes$abbrev
+
 # 538 data
 if(!file.exists("bad-drivers.csv"))
     system("wget https://raw.githubusercontent.com/fivethirtyeight/data/master/bad-drivers/bad-drivers.csv")
@@ -6,3 +10,5 @@ crashes <- read.csv("bad-drivers.csv")
 rownames(crashes) <- crashes[,1]
 crashes[,3:6] <- crashes[,3:6]*crashes[,2]/100
 colnames(crashes)[-1] <- c("total", "speeding", "alcohol", "not_distracted", "no_previous", "ins_premium", "ins_losses")
+crashes <- crashes[,-1]
+crashes$abbrev <- abbrev
